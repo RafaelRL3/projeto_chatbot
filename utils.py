2172a181ec64fd82717bf5c4038c5e3edb7108f5
RafaelRL3 <rrl3@cin.ufpe.br>
2022-04-21 11:14:10 -0300

    Rdt with utils

---
+++ utils.py
@@ -0,0 +1,26 @@
+def checkSum(self, data: str) -> int:
+        print("checa o checksum")
+
+        #RFC 1071
+        addr = 0 
+        # Copute Internet Checksum for "count" bytes, begining at location "addr"
+        Sum = 0
+
+        count = len(data)
+
+        while (count > 1):
+            # inner loop
+            Sum += data[addr] << 8 + data[addr+1]  # index do byte 
+            addr += 2
+            count -= 2
+
+        # add left-over byte, if any
+        if (count > 0):
+            Sum += data[addr]
+
+        # fold 32-bit Sum to 16 bits
+        while (Sum>>16):
+            Sum = (Sum & 0xffff) + (Sum >> 16)
+
+        checksum = ~Sum
+        return checksum
